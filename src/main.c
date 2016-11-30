2c668f11a47ab712a8db46925625a976bd714235
tinyco <tiny.code.editor@gmail.com>
2016-12-01 05:10:15 +0900

    day2, file read.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+void file_print(char* filename) {
+  FILE *fp;
+  char str[10];
+  if((fp = fopen(filename, "r")) == NULL) {
+    printf("[error]can't open\n");
+    return;
+  }
+  while(fgets(str, sizeof(str), fp)) {
+    printf("%s", str);
+  }
+  fclose(fp);
+}
+
 int main(int argc, char *argv[]) {
   printf("\e[2J\e[1;1H");//clear
-  printf("\e[31m\e[47m");//color
-  printf("hello,world.\n");
-  printf("\e[39m\e[49m");//reset
-  for(int i=0; i<argc; i++) {
-    printf("receive > %s \n",argv[i]);
+  if(argc != 2) {
+    printf("[error]illegal args\n");
+  } else {
+    file_print(argv[1]);
   }
 }
