15546516a5e53d0ebd922f0fc79e01d7d40591d6
tinyco <tiny.code.editor@gmail.com>
2016-12-01 03:46:23 +0900

    day1, clear console.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+  printf("\e[2J\e[1;1H");//clear
+  printf("\e[31m\e[47m");//color
   printf("hello,world.\n");
+  printf("\e[39m\e[49m");//reset
   for(int i=0; i<argc; i++) {
     printf("receive > %s \n",argv[i]);
   }
-  printf("don't receive > %s \n",argv[100]);
 }
