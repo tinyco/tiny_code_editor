91ddfb9e74fb044f66c2c6e8a83170581097ac72
tinyco <tiny.code.editor@gmail.com>
2016-12-01 02:49:58 +0900

    day1, get args.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
   printf("hello,world.\n");
+  for(int i=0; i<argc; i++) {
+    printf("receive > %s \n",argv[i]);
+  }
+  printf("don't receive > %s \n",argv[100]);
 }
