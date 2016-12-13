51514e7d44ca288ede1ff9e90d12bcf9e4908291
tinyco <tiny.code.editor@gmail.com>
2016-12-14 06:53:04 +0900

    day13, exit with ctrl + q.

--- src/command/command_parse.c
+++ src/command/command_parse.c
@@ -8,22 +8,21 @@ command command_parse(mbchar key)//PUBLIC;
   static enum ControlKeyFlag flag = 0;
   command cmd;
   cmd.command_key = NONE;
+  cmd.command_value = key;
   if(key[0] == 0x1B && flag == NOT_CTRL) {
     flag = ALLOW_1;
   } else if (key[0] == 0x5B && flag == ALLOW_1) {
     flag = ALLOW_2;
   } else if (flag == ALLOW_2) {
-    if(key[0] == 0x41) cmd.command_key = UP;
-    if(key[0] == 0x42) cmd.command_key = DOWN;
-    if(key[0] == 0x43) cmd.command_key = RIGHT;
-    if(key[0] == 0x44) cmd.command_key = LEFT;
+    /**/ if(key[0] == 0x41) {cmd.command_key = UP;}
+    else if(key[0] == 0x42) {cmd.command_key = DOWN;}
+    else if(key[0] == 0x43) {cmd.command_key = RIGHT;}
+    else if(key[0] == 0x44) {cmd.command_key = LEFT;}
     flag = NOT_CTRL;
   } else {
+    /**/ if(key[0] == 0x11) {cmd.command_key = EXIT;}
+    /**/ else               {cmd.command_key = INSERT;}
     flag = NOT_CTRL;
-    cmd.command_key = INSERT;
   }
-  cmd.command_value = key;
-  if(!strcmp((const char*)key, "e"))cmd.command_key = EXIT;
-
   return cmd;
 }
