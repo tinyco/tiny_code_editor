f3d41d27048608087b741a31576b3f7e0f90b648
tinyco <tiny.code.editor@gmail.com>
2016-12-17 03:13:32 +0900

    day16, fput -> fwrite.

--- src/file/writer.c
+++ src/file/writer.c
@@ -12,18 +12,16 @@ void file_write(const char* filepath, text* head)//PUBLIC;
   text* current_text = head;
 	line* current_line = head->line;
 
-  unum i;
 	while(current_text)
   {
     current_line = current_text->line;
   	while(current_line)
     {
-      i = 0;
-      while(i < current_line->byte_count)
-      {
-        fputc(current_line->string[i], fp);
-        i++;
-      }
+      fwrite(
+        current_line->string,
+        sizeof current_line->string[0],
+        current_line->byte_count,
+        fp);
 	  	current_line = current_line->next;
     }
 		current_text = current_text->next;
