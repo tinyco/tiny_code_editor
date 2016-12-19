79d7d74abba549aac8cf497fa27e0a6e6a598472
tinyco <tiny.code.editor@gmail.com>
2016-12-20 02:40:26 +0900

    day18, fix code formatter.

--- src/code_format.sh
+++ src/code_format.sh
@@ -1,4 +1,4 @@
 find . | grep '\.c$' | sed 's/\(.*\)/ expand -t 2 \1 > \1.bak /g' | sh
 find . | grep '\.c\.bak$' | sed 's/\(.*\)\.bak/ mv \1.bak \1 /g' | sh
-find . | grep '\.c$' | sed 's/\(.*\)/ clang-format \1 > \1.bak /g' | sh
-find . | grep '\.c\.bak$' | sed 's/\(.*\)\.bak/ mv \1.bak \1 /g' | sh
+find . | grep '\.c$' | sed 's/\(.*\)/ echo "" >> \1 /g' | sh
+find . | grep '\.c$' | sed 's/\(.*\)/ clang-format -i \1 /g' | sh
