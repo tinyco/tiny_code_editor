a92a8528cb82b8125f745633e66672ae9b61527b
tinyco <tiny.code.editor@gmail.com>
2016-12-25 06:43:44 +0900

    day22, change code format.

--- src/command/command_perform.c
+++ src/command/command_perform.c
@@ -44,7 +44,8 @@ void command_perform(command command, context *context) // PUBLIC;
   case INSERT: {
     uint byte;
     lines *head = lines_select_position_y(context->lines, context->cursor.position_y);
-    mutable_string *mutable_string = mutable_string_select_position_x(head->mutable_string, context->cursor.position_x, &byte);
+    mutable_string *mutable_string =
+        mutable_string_select_position_x(head->mutable_string, context->cursor.position_x, &byte);
     insert_utf8char(mutable_string, byte, command.command_value);
     (*context).cursor.position_x += 1;
   } break;
@@ -52,20 +53,23 @@ void command_perform(command command, context *context) // PUBLIC;
     if (context->cursor.position_x > 1) {
       uint byte;
       lines *head = lines_select_position_y(context->lines, context->cursor.position_y);
-      mutable_string *mutable_string = mutable_string_select_position_x(head->mutable_string, context->cursor.position_x - 1, &byte);
+      mutable_string *mutable_string =
+          mutable_string_select_position_x(head->mutable_string, context->cursor.position_x - 1, &byte);
       delete_utf8char(mutable_string, byte);
       (*context).cursor.position_x -= 1;
     } else if (context->cursor.position_y > 1) {
       lines *head = lines_select_position_y(context->lines, context->cursor.position_y - 1);
       lines_combine_next(head);
-      (*context).cursor.position_x = lines_select_position_y((*context).lines, (*context).cursor.position_y - 1)->position_count;
+      (*context).cursor.position_x =
+          lines_select_position_y((*context).lines, (*context).cursor.position_y - 1)->position_count;
       (*context).cursor.position_y -= 1;
     }
   } break;
   case ENTER: {
     uint byte;
     lines *head = lines_select_position_y(context->lines, context->cursor.position_y);
-    mutable_string *mutable_string = mutable_string_select_position_x(head->mutable_string, context->cursor.position_x, &byte);
+    mutable_string *mutable_string =
+        mutable_string_select_position_x(head->mutable_string, context->cursor.position_x, &byte);
     lines_divide(head, mutable_string, byte, command.command_value);
     (*context).cursor.position_x = 1;
     (*context).cursor.position_y += 1;
--- src/file/writer.c
+++ src/file/writer.c
@@ -14,7 +14,8 @@ void file_write(const char *filepath, lines *head) // PUBLIC;
   while (current_lines) {
     current_mutable_string = current_lines->mutable_string;
     while (current_mutable_string) {
-      fwrite(current_mutable_string->string, sizeof current_mutable_string->string[0], current_mutable_string->byte_count, fp);
+      fwrite(current_mutable_string->string, sizeof current_mutable_string->string[0],
+             current_mutable_string->byte_count, fp);
       current_mutable_string = current_mutable_string->next;
     }
     current_lines = current_lines->next;
