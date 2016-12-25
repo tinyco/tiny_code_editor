1c76aac5deea2c70ad040d97a21ef736e869d4e0
tinyco <tiny.code.editor@gmail.com>
2016-12-25 20:02:48 +0900

    space.

--- src/render/context.c
+++ src/render/context.c
@@ -19,7 +19,7 @@ typedef struct _context {
 
 void context_read_file(context *context, char *filename) // PUBLIC;
 {
-  (*context).filename = malloc(strlen(filename)+1);
+  (*context).filename = malloc(strlen(filename) + 1);
   strcpy((*context).filename, filename);
   (*context).lines = file_read((*context).filename);
 }
