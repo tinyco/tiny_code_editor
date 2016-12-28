cd72fcc749ec63e78471cd33281095452f20cd46
equal-l2 <equal-l2@users.noreply.github.com>
2016-12-29 08:51:54 +0900

    Fix warning by clang-static-analyzer

--- src/file/writer.c
+++ src/file/writer.c
@@ -9,10 +9,9 @@ void file_write(const char *filepath, lines *head) // PUBLIC;
   }
 
   lines *current_lines = head;
-  mutable_string *current_mutable_string = head->mutable_string;
 
   while (current_lines) {
-    current_mutable_string = current_lines->mutable_string;
+    mutable_string *current_mutable_string = current_lines->mutable_string;
     while (current_mutable_string) {
       fwrite(current_mutable_string->string, sizeof current_mutable_string->string[0],
              current_mutable_string->byte_count, fp);
